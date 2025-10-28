777e5b1fc6d8d22dd1816bcf1189faa41c6ca0bb
Pulok Das <pulokdas@zohomail.com>
2025-10-28 10:31:01 +0530

    Done

--- my-learn-link/vite.config.ts
+++ my-learn-link/vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Portfolio/", // 👈 Add this line — your repo name (case-sensitive!)
+  base: "/my-learn-link", // 👈 Add this line — your repo name (case-sensitive!)
   server: {
     host: "::",
     port: 8080,
